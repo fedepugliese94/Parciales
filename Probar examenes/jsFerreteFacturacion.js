9fbf459ccbbf751d0e4f19cafb22225ffd0c5a59
fedepugliese94 <fedepugliese94@gmail.com>
2016-08-02 21:57:25 -0300

    Ejercicio de examen 6 hecho Re hacer hasta que salga solo!! Hecho en ehercicio FERRETE FACTURACION.

--- Probar examenes/jsFerreteFacturacion.js
+++ Probar examenes/jsFerreteFacturacion.js
@@ -5,9 +5,40 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	var base=document.getElementById('PrecioUno').value;
-	var perimetro= parseInt(base*4);
-	alert(perimetro);
+	/*var contador=1;
+	var dato;
+	var acumulador=0;
+	dato=parseInt(dato);
+	acumulador=parseInt(acumulador);
+	var mayor=0;
+	var menor=99;
+	mayor=parseInt(mayor);
+	menor=parseInt(menor);
+	while(contador<5)
+		{
+			dato=prompt("Ingrese dato " + contador );
+			while(dato<=0 || isNaN(dato))
+				{
+					dato=prompt("Ingrese dato valido " + contador);
+				}
+			contador++
+			acumulador=parseInt(acumulador)+parseInt(dato);
+
+			if (dato>mayor) 
+				{
+					mayor=dato;
+				}
+			if (dato<menor) 
+				{
+					menor=dato;
+				}
+
+		}
+
+	alert("El total es " + acumulador);
+	alert("El numero mayor es " + mayor);
+	alert("El numero menor es " + menor);
+	*/	
 
 }
 function Promedio () 
@@ -16,5 +47,30 @@ function Promedio ()
 }
 function PrecioFinal () 
 {
-	
+	/*var diaUno=prompt("Introducir ventas del dia");
+	var diaDos=prompt("Introducir ventas del dia");
+	var diaTres=prompt("Introducir ventas del dia");
+	var diaCuatro=prompt("Introducir ventas del dia");
+	var diaCinco=prompt("Introducir ventas del dia");
+	var diaSeis=prompt("Introducir ventas del dia");
+	var diaSiete=prompt("Introducir ventas del dia");
+	var diaOcho=prompt("Introducir ventas del dia");
+	var diaNueve=prompt("Introducir ventas del dia");
+	var diaDies=prompt("Introducir ventas del dia");
+	var diaOnce=prompt("Introducir ventas del dia");
+	var diaDoce=prompt("Introducir ventas del dia");
+	var diaTresce=prompt("Introducir ventas del dia");
+	var diaDosce=prompt("Introducir ventas del dia");
+	var diaCatorce=prompt("Introducir ventas del dia");
+	var diaQuince=prompt("Introducir ventas del dia");
+	var diaDieciseis=prompt("Introducir ventas del dia");
+	var diaDiecisiete=prompt("Introducir ventas del dia");
+	var diaDieciocho=prompt("Introducir ventas del dia");
+	var diaDiecinueve=prompt("Introducir ventas del dia");
+	var diaVeinte=prompt("Introducir ventas del dia");
+	var diaVeintiuno=prompt("Introducir ventas del dia");
+	var diaVeintidos=prompt("Introducir ventas del dia");
+	var diaVeintitres=prompt("Introducir ventas del dia");
+	var diaVeinticuatro=prompt("Introducir ventas del dia");
+	*/
 }
